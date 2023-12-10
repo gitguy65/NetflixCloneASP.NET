61124beb7c778845e16a11dee7ef8be8df84282b
I <I@DESKTOP-H53MDKA>
2023-12-10 07:26:00 -0800

    Add project files.

---
+++ NetflixCloneASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetflixCloneASP.NET.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
